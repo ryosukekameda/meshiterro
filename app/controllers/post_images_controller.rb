3ca6827c37d9f13d4048493d4e0e8cea2fccc1a2
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-08-02 09:15:57 +0000

    meshiterro 08021815

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,11 +4,10 @@ class PostImagesController < ApplicationController
   end
   
   def create
-    
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
 
   def index
@@ -16,6 +15,13 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+  end
+  
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
   end
   
   private
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,12 +2,13 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
-  def get_image
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attached(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    else
-      'no_image.jpg'
+    
+      
     end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
